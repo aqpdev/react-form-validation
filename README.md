1ac3d0b89134fd2526a9adae29c118000d3b03ca
apqdev <support@evolutionscript.com>
2023-09-07 01:16:10 -0500

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,127 @@
 # react-form-validation
+
+React hook to validate form values
+
+## Installation
+
+```bash
+npm i react-form-validation
+```
+
+## Usage
+
+```javascript
+import React from "react"
+import {useFormValidation} from "react-form-validation";
+
+export default function Home() {
+    const {handleForm, hasErrors, showError} = useFormValidation([
+        {
+            field_name: "username",
+            rules: "required|min_length[5]|alpha_dash",
+            errors: {
+                required: "Username is required.",
+                min_length: "Username must contains at least 5 characters.",
+                alpha_dash: "Username may only contain alpha-numeric characters, underscores, and dashes."
+            }
+        },
+        {
+            field_name: "password",
+            rules: "required|min_length[6]|alpha_with_upper_numeric_special",
+            errors: {
+                required: "Password is required.",
+                min_length: "Password must contains at least 6 characters.",
+                alpha_with_upper_numeric_special: "Password must contains letters (lowercase and uppercase), numbers and special characters.",
+            }
+        },
+        {
+            field_name: "confirm_password",
+            rules: "required|matches[password]",
+            errors: {
+                required: "Confirm your password.",
+                matches: "Passwords do not match.."
+            }
+        },
+        {
+            field_name: "email",
+            rules: "required|valid_email",
+            errors: {
+                required: "Enter your email address.",
+                valid_email: "Email address is not valid."
+            }
+        }
+    ]);
+
+    const myHandle = (data) => {
+        //Data returns a JSON of form elements
+        console.log(data);
+    }
+
+    return (
+        <form onSubmit={(e) => handleForm(e, myHandle)}>
+            <div className="mb-3">
+                <label className="form-label">Username</label>
+                <input type="text" name="username" className="form-control"/>
+                {hasErrors('username') && <div className="text-danger">{showError('username')}</div>}
+            </div>
+
+            <div className="mb-3">
+                <label className="form-label">Password</label>
+                <input type="password" name="password" className="form-control"/>
+                {hasErrors('password') && <div className="text-danger">{showError('password')}</div>}
+            </div>
+
+            <div className="mb-3">
+                <label className="form-label">Confirm Password</label>
+                <input type="password" name="confirm_password" className="form-control"/>
+                {hasErrors('confirm_password') && <div className="text-danger">{showError('confirm_password')}</div>}
+            </div>
+
+            <div className="mb-3">
+                <label className="form-label">Email</label>
+                <input type="text" name="email" className="form-control"/>
+                {hasErrors('email') && <div className="text-danger">{showError('email')}</div>}
+            </div>
+
+            <div>
+                <button className="btn btn-primary">Submit</button><br/>
+                <button type="button" className="btn btn-primary">Button blocked</button><br/>
+            </div>
+        </form>
+    )
+}
+```
+
+## Rules Reference
+
+| Rule                             | Parameter | Description                                                                                                                                                           | Example                     |
+|----------------------------------|-----------|-----------------------------------------------------------------------------------------------------------------------------------------------------------------------|-----------------------------|
+| required                         | No        | Returns error if the form element is empty.                                                                                                                           |                             |
+| matches                          | Yes       | Returns error if the form element does not match the one in the parameter.                                                                                            | matches[form_item]          |
+| regex_match                      | Yes       | Returns error is the form element does not match the regular expresion.                                                                                               | regex_match[/regex/]        |
+| differs                          | Yes       | Returns error if the form element does not differ from the one in the parameter.                                                                                      | differs[form_item]          |
+| min_length                       | Yes       | Returns error if the form element is shorter than the parameter value.                                                                                                | min_length[3]               |
+| max_length                       | Yes       | Returns error if the form element is longer than the parameter value.                                                                                                 | max_length[12]              |
+| exact_length                     | Yes       | Returns error if the form element is not exactly the parameter value.                                                                                                 | exact_length[8]             |
+| greater_than                     | Yes       | Returns error if the form element is less than or equal to the parameter value or not numeric.                                                                        | greater_than[8]             |
+| greater_than_equal_to            | Yes       | Returns error if the form element is less than the parameter value, or not numeric.                                                                                   | greater_than_equal_to[8]    |
+| less_than                        | Yes       | Returns error if the form element is greater than or equal to the parameter value or not numeric.                                                                     | less_than[8]                |
+| less_than_equal_to               | Yes       | Returns error if the form element is greater than the parameter value, or not numeric.                                                                                | less_than_equal_to[8]       |
+| in_list                          | Yes       | Returns error if the form element is not within a predetermined list.                                                                                                 | in_list[red,blue,green]     |
+| alpha                            | No        | Returns error if the form element contains anything other than alphabetical characters.                                                                               |                             |
+| alpha_numeric                    | No        | Returns error if the form element contains anything other than alpha-numeric characters.                                                                              |                             |
+| alpha_numeric_spaces             | No        | Returns error if the form element contains anything other than alpha-numeric characters or spaces. Should be used after trim to avoid spaces at the beginning or end. |                             |
+| alpha_dash                       | No        | Returns error if the form element contains anything other than alpha-numeric characters, underscores or dashes.                                                       |                             |
+| alpha_and_numeric                | No        | Returns error if the form element does not contain letters and numbers.                                                                                               |                             |
+| alpha_with_upper_and_numeric     | No        | Returns error if the form element does not contain letters (lowercase and uppercase) and numbers.                                                                     |                             |
+| alpha_with_upper_numeric_special | No        | Returns error if the form element does not contain letters (lowercase and uppercase), numbers and special characters.                                                 |                             |
+| numeric                          | No        | Returns error if the form element contains anything other than numeric characters.                                                                                    |                             |
+| integer                          | No        | Returns error if the form element contains anything other than an integer.                                                                                            |                             |
+| decimal                          | No        | Returns error if the form element contains anything other than a decimal number.                                                                                      |                             |
+| is_natural                       | No        | Returns error if the form element contains anything other than a natural number: 0, 1, 2, 3, etc.                                                                     |                             |
+| is_natural_no_zero               | No        | Returns error if the form element contains anything other than a natural number, but not zero: 1, 2, 3, etc.                                                          |                             |
+| valid_url                        | No        | Returns error if the form element does not contain a valid URL.                                                                                                       |                             |
+| valid_email                      | No        | Returns error if the form element does not contain a valid email address.                                                                                             |                             |
+| valid_ip                         | No        | Returns error if the form element does not contain a valid IP address (‘ipv4’ or ‘ipv6’).                                                                             |                             |
+| if_requires                      | Yes       | Returns error if the form element is filled and the other form element (parameter) is empty.                                                                          | if_requires[form_item]      |
+| if_use_requires                  | Yes       | Returns error if the form element is equal to first parameter and the other form element (second parameter) is empty.                                                 | if_requires[user,form_item] |
---
+++ index.js
@@ -0,0 +1,340 @@
+import React, {useState} from "react"
+
+export function useFormValidation(rules)
+{
+    const [errorsList, setErrors] = useState({});
+    const [myForm, setMyForm] = useState({});
+    let formElement = {};
+    let formErrors = {};
+    let formInputs = {};
+    const parseForm = () => {
+        formErrors = {};
+        const formItems = new FormData(formElement);
+        formItems.forEach((field_value, field_name) => {
+            formInputs[field_name] = field_value;
+            validate(field_name);
+        });
+        setErrors(formErrors);
+        setMyForm(formElement);
+    }
+
+    const handleForm = (event, callbackFunction) =>
+    {
+        event.preventDefault();
+        formElement = event.target;
+        parseForm();
+        if(Object.keys(formErrors).length === 0){
+            callbackFunction(formInputs)
+        }
+
+    }
+
+    const validate = (field_name) =>     {
+
+        const item = rules.find(e => e.field_name === field_name);
+        if(typeof item === "undefined"){
+            return true;
+        }
+        const element = formElement[field_name];
+
+        let validRegex = '';
+        const splitted = item.rules.split('|');
+
+
+        for(var i=0; i<splitted.length; i++){
+            let propertyName = splitted[i];
+            let propertyValue = 0;
+
+            validRegex = /^[a-zA-Z0-9-_]+\[+(.*)+\]*$/;
+            if(propertyName.match(validRegex)){
+                const subSplit = propertyName.split(/\[(.*)\]/);
+                propertyName = subSplit[0];
+                propertyValue = subSplit[1];
+            }
+
+            if(propertyName === 'required') {
+                if(element.type === 'checkbox'){
+                    if(!element.checked){
+                        formErrors[field_name] = item.errors[propertyName];
+                        return false;
+                    }
+                }else if (element.value === "") {
+                    formErrors[field_name] = item.errors[propertyName];
+                    return false;
+                }
+            }
+
+            if(element.value === ""){
+                return true;
+            }
+
+            if(propertyName === 'matches') {
+                const inputValidator = formElement[propertyValue];
+                if(typeof inputValidator === "undefined"){
+                    formErrors[field_name] = item.errors[propertyName];
+                    return false;
+                }else{
+                    if (inputValidator.value !== element.value) {
+                        formErrors[field_name] = item.errors[propertyName];
+                        return false;
+                    }
+                }
+            }else if(propertyName === 'regex_match'){
+                if(!element.value.match(eval(propertyValue))){
+                    formErrors[field_name] = item.errors[propertyName];
+                    return false;
+                }
+            }else if(propertyName === 'differs'){
+                const inputValidator = formElement[propertyValue];
+                if(typeof inputValidator !== "undefined"){
+                    if (inputValidator.value === element.value) {
+                        formErrors[field_name] = item.errors[propertyName];
+                        return false;
+                    }
+                }
+            }else if(propertyName === 'min_length'){
+                if(element.value.length < propertyValue){
+                    formErrors[field_name] = item.errors[propertyName];
+                    return false;
+                }
+            }else if(propertyName === 'max_length'){
+                if(element.value.length > propertyValue){
+                    formErrors[field_name] = item.errors[propertyName];
+                    return false;
+                }
+            }else if(propertyName === 'exact_length'){
+                if(element.value.length !== parseInt(propertyValue)){
+                    formErrors[field_name] = item.errors[propertyName];
+                    return false;
+                }
+            }else if(propertyName === 'greater_than'){
+                if(!isNumeric(element.value)){
+                    formErrors[field_name] = item.errors[propertyName];
+                    return false;
+                }
+                if(parseFloat(element.value) <= parseFloat(propertyValue)){
+                    formErrors[field_name] = item.errors[propertyName];
+                    return false;
+                }
+            }else if(propertyName === 'greater_than_equal_to'){
+                if(!isNumeric(element.value)){
+                    formErrors[field_name] = item.errors[propertyName];
+                    return false;
+                }
+
+                if(parseFloat(element.value) < parseFloat(propertyValue)){
+                    formErrors[field_name] = item.errors[propertyName];
+                    return false;
+                }
+            }else if(propertyName === 'less_than'){
+                if(!isNumeric(element.value)){
+                    formErrors[field_name] = item.errors[propertyName];
+                    return false;
+                }
+                if(parseFloat(element.value) >= parseFloat(propertyValue)){
+                    formErrors[field_name] = item.errors[propertyName];
+                    return false;
+                }
+            }else if(propertyName === 'less_than_equal_to') {
+                if (!isNumeric(element.value)) {
+                    formErrors[field_name] = item.errors[propertyName];
+                    return false;
+                }
+                if (parseFloat(element.value) > parseFloat(propertyValue)) {
+                    formErrors[field_name] = item.errors[propertyName];
+                    return false;
+                }
+            }else if(propertyName === 'in_list'){
+                const subPropertySplit = propertyValue.split(',');
+                var in_list_found = false;
+                for(var il = 0; il < subPropertySplit.length; il++) {
+                    if(subPropertySplit[il] === element.value){
+                        in_list_found = true;
+                    }
+                }
+                if(in_list_found !== true){
+                    formErrors[field_name] = item.errors[propertyName];
+                    return false;
+                }
+            }else if(propertyName === 'alpha'){
+                validRegex = /^[a-zA-Z]*$/;
+                if(!element.value.match(validRegex)){
+                    formErrors[field_name] = item.errors[propertyName];
+                    return false;
+                }
+            }else if(propertyName === 'alpha_spaces'){
+                validRegex = /^[a-zA-Z/\s]*$/;
+                if(!element.value.match(validRegex)){
+                    formErrors[field_name] = item.errors[propertyName];
+                    return false;
+                }
+            }else if(propertyName === 'alpha_numeric') {
+                validRegex = /^[a-zA-Z0-9]*$/;
+                if (!element.value.match(validRegex)) {
+                    formErrors[field_name] = item.errors[propertyName];
+                    return false;
+                }
+            }else if(propertyName === 'alpha_numeric_spaces'){
+                validRegex = /^[a-zA-Z0-9/\s]*$/;
+                if(!element.value.match(validRegex)){
+                    formErrors[field_name] = item.errors[propertyName];
+                    return false;
+                }
+            }else if(propertyName === 'alpha_dash') {
+                validRegex = /^[a-zA-Z0-9-_]*$/;
+                if (!element.value.match(validRegex)) {
+                    formErrors[field_name] = item.errors[propertyName];
+                    return false;
+                }
+            }else if(propertyName === 'alpha_and_numeric'){
+                validRegex = /^(?=.*[a-zA-Z])(?=.*[0-9])/;
+                if (!element.value.match(validRegex)) {
+                    formErrors[field_name] = item.errors[propertyName];
+                    return false;
+                }
+            }else if(propertyName === 'alpha_with_upper_and_numeric'){
+                validRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])/;
+                if (!element.value.match(validRegex)) {
+                    formErrors[field_name] = item.errors[propertyName];
+                    return false;
+                }
+            }else if(propertyName === 'alpha_with_upper_numeric_special'){
+                validRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~])/;
+                if (!element.value.match(validRegex)) {
+                    formErrors[field_name] = item.errors[propertyName];
+                    return false;
+                }
+            }else if(propertyName === 'numeric'){
+                if(!isNumeric(element.value)){
+                    formErrors[field_name] = item.errors[propertyName];
+                    return false;
+                }
+            }else if(propertyName === 'integer'){
+                if(!isInteger(element.value)){
+                    formErrors[field_name] = item.errors[propertyName];
+                    return false;
+                }
+            }else if(propertyName === 'decimal'){
+                if(!isNumeric(element.value)){
+                    formErrors[field_name] = item.errors[propertyName];
+                    return false;
+                }
+                if(parseFloat(element.value) === parseInt(element.value)){
+                    formErrors[field_name] = item.errors[propertyName];
+                    return false;
+                }
+            }else if(propertyName === 'is_natural'){
+                if(!isInteger(element.value)){
+                    formErrors[field_name] = item.errors[propertyName];
+                    return false;
+                }else if(parseInt(element.value) < 0){
+                    formErrors[field_name] = item.errors[propertyName];
+                    return false;
+                }
+            }else if(propertyName === 'is_natural_no_zero'){
+                if(!isInteger(element.value)){
+                    formErrors[field_name] = item.errors[propertyName];
+                    return false;
+                }else if(parseInt(element.value) <= 0){
+                    formErrors[field_name] = item.errors[propertyName];
+                    return false;
+                }
+            }else if(propertyName === 'valid_email'){
+                validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+                if(!element.value.match(validRegex)){
+                    formErrors[field_name] = item.errors[propertyName];
+                    return false;
+                }
+            }else if(propertyName === 'valid_url'){
+                try{
+                    let validURlVal = new URL(element.value);
+                }catch (error){
+                    formErrors[field_name] = item.errors[propertyName];
+                    return false;
+                }
+            }else if(propertyName === 'valid_ip'){
+                validRegex = /((^\s*((([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))\s*$)|(^\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\s*$))/;
+                if(!element.value.match(validRegex)){
+                    formErrors[field_name] = item.errors[propertyName];
+                    return false;
+                }
+            }else if(propertyName === "if_requires"){
+                const inputValidator = formElement[propertyValue];
+                if(typeof inputValidator === "undefined"){
+                    formErrors[propertyValue] = item.errors[propertyName];
+                    return false;
+                }else{
+                    if (inputValidator.value === "") {
+                        formErrors[propertyValue] = item.errors[propertyName];
+                        return false;
+                    }
+                }
+            }else if(propertyName === "if_use_requires"){
+                const subPropertySplit = propertyValue.split(',');
+                if(element.value === subPropertySplit[0]){
+                    const inputValidator = formElement[subPropertySplit[1]];
+                    if(typeof inputValidator === "undefined"){
+                        formErrors[subPropertySplit[1]] = item.errors[propertyName];
+                        return false;
+                    }else{
+                        if (inputValidator.value === "") {
+                            formErrors[subPropertySplit[1]] = item.errors[propertyName];
+                            return false;
+                        }
+                    }
+                }
+                //Update this
+                const propertyRequired = element.dataset.requires;
+                if(propertyRequired !== undefined){
+                    if(element.value === propertyValue){
+                        const inputValidator = this.formElements[propertyRequired];
+                        if(inputValidator.value === '' || inputValidator.value === null){
+                            inputValidator.classList.add('is-invalid');
+                            return false;
+                        }
+                    }
+                }
+            }
+
+        }
+        return true;
+
+    }
+
+    const isNumeric = (val) => {
+        return !isNaN(val);
+    }
+
+    const isInteger = (val) => {
+        if(!isNumeric(val)){
+            return false;
+        }
+        return parseInt(val) === parseFloat(val);
+    }
+
+    const uniqID = (prefix="",random=true) =>
+    {
+        const sec = Date.now() * 1000 + Math.random() * 1000;
+        const id = sec.toString(16).replace(/\./g, "").padEnd(14, "0");
+        return `${prefix}${id}${random ? `.${Math.trunc(Math.random() * 100000000)}`:""}`;
+    }
+
+    const hasErrors = (field_name) => {
+        return (errorsList[field_name] !== undefined);
+    }
+    const showError = (field_name) => {
+        if(errorsList[field_name] !== undefined){
+            const element = myForm[field_name];
+            if(typeof element.id === "undefined" || element.id === ""){
+                element.id = uniqID(field_name);
+            }
+            const elementByID = document.getElementById(element.id);
+            elementByID.scrollIntoView({block: "end", behavior: "smooth"})
+            elementByID.focus();
+            return errorsList[field_name];
+        }else{
+            return null;
+        }
+    }
+    return {handleForm, hasErrors, showError};
+}
